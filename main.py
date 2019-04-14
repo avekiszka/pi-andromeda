d5e8fd05c731e9b461e18d302ce97ecd6599f623
avekiszka <45669939+avekiszka@users.noreply.github.com>
2019-04-14 10:11:01 -0400

    Net tools implemented

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 # start import statements
 from scapy.all import *
+import os
 # end import statements
+os.system("clear")
 print("This is pi-Andromenda project")
 def get_choice(help_text="Choose(1/2/3): "):
     return int(input(help_text))
 def get_dst(help_text="Podaj Adres docelowy: "):
     return input(help_text)
+def get_dp(help_text="Podaj numer portu docelowego: "):
+    return int(input(help_text))
 def net_tools():
-    ans, unans = sr(IP(dst=get_dst())/ICMP())
-    ans[0][0].sprintf("Host %IP.src% is alive.")
+    print("Wybierz narzedzie, ktore chcesz uruchomic: \n1) ICMP Ping\n2) Traceroute\n3) TCP Ping\n4) UDP Ping")
+    ch1 = get_choice("(1/2/3/4): ")
+    if ch1 == 1:
+        os.system("clear")
+        print("---ICMP-Ping---")
+        p = IP(dst=get_dst())/ICMP()
+        ans, unans = sr(p)
+        print(ans[0][1].sprintf("Host %IP.src% is alive."))
+    elif ch1 == 2:
+        os.system("clear")
+        print("---Traceroute---")
+    elif ch1 == 3:
+        os.system("clear")
+        print("---TCP-Ping---")
+        ans,uans =sr(IP(dst=get_dst())/TCP(dport=get_dp(), flags="S"))
+        print(ans[0][1].sprintf("Host %IP.src% is alive"))
+    elif ch1 == 4:
+        os.system("clear")
+        print("---UDP-Ping---")
+        ans, uans = sr(IP(dst=get_dst())/UDP(dport=0))
+        print(ans[0][1].sprintf("Host %IP.src% is alive"))
 
 print("Wybierz Kategorie: \n1) Network Tools\n2) Network Scans\n3) Network Attacks")
 cho1 = get_choice()
